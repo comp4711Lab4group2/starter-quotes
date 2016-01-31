62a1d13248fd6cfa325f6ff8acf44089b859b49e
Evgeni Maiseyeu <emaiseyeu@my.bcit.ca>
2016-01-31 15:30:19 -0800

    fixed first link

---
+++ application/controllers/First.php
@@ -0,0 +1,20 @@
+<?php
+
+class First extends Application {
+	function __construct()
+	{
+		parent::__construct();
+	}
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+	function index()
+        {
+		$this->data['pagebody'] = 'justone';
+		$record = $this->quotes->first();
+		$this->data = array_merge($this->data, $record);
+		
+		$this->render();
+	}
+          
+}
