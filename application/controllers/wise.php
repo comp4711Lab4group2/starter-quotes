cd18d37cc845c4fc5882741a5a317347407bed43
Himesh Badlani <Himesh Badlani>
2016-01-31 18:19:47 -0800

    link8

---
+++ application/controllers/wise.php
@@ -0,0 +1,18 @@
+<?php
+
+class Wise extends Application
+{
+    function __construct()
+    {
+	parent::__construct();
+    }
+
+    function bingo()
+    {
+	$record = $this->quotes->get(6);
+	$this->data = array_merge($this->data, $record);
+	$this->data['pagebody'] = 'justone';
+
+	$this->render();
+    }
+}
